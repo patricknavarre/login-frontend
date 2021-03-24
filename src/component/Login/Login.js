34d7c728a6d9a60e6e1379ebd0bbca74dd1de01d
Patrick Navarre <patrick.navarre@codeimmersives.com>
2021-03-23 21:10:00 -0400

    login complete

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react'
 
 export class Login extends Component {
+    state = {
+        email: "",
+        password: "",
+    };
+
+    handleLogin = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
     render() {
+        const { email, password } = this.state;
         return (
-            <div>
-                Login Page
-            </div>
-        )
+          <div className="form-body">
+            <main className="form-signin">
+              <form onSubmit={this.handleLoginSubmit}>
+                <h1 className="h3 mb-3 fw-normal">Please Login!</h1>
+                <label htmlFor="inputEmail" className="visually-hidden">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  id="inputEmail"
+                  className="form-control"
+                  placeholder="Email address"
+                  required
+                  autoFocus
+                  name="email"
+                  value={email}
+                  onChange={this.handleLogin}
+                />
+                <label htmlFor="inputPassword" className="visually-hidden">
+                  Password
+                </label>
+                <input
+                  //type="password"
+                  type="text"
+                  id="inputPassword"
+                  className="form-control"
+                  placeholder="Password"
+                  required
+                  name="password"
+                  value={password}
+                  onChange={this.handleLogin}
+                />
+                <button className="w-100 btn btn-lg btn-primary" type="submit">
+                  Login
+                </button>
+              </form>
+            </main>
+          </div>
+        );
+      }
     }
-}
 
 export default Login
